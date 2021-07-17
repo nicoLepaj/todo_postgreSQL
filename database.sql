41b728b559e850e0a04200128d3bdf0cf22a7f6e
Nicolas <nicolas.lepaj@gmail.com>
2021-07-18 01:31:35 +0200

    comment in database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE perntodo
+-- CREATE DATABASE perntodo
 
-CREATE TABLE todo( todo_id SERIAL PRIMARY KEY, description VARCHAR(255) )
+CREATE TABLE todo( todo_id SERIAL PRIMARY KEY, description VARCHAR(255) ); 
