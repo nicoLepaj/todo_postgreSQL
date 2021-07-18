46ccb3f0ad4cc41dc3734f6dbfdc0fb7350155b5
Nicolas <nicolas.lepaj@gmail.com>
2021-07-18 04:21:20 +0200

    log

--- client/src/components/ListTodos.vue
+++ client/src/components/ListTodos.vue
@@ -44,6 +44,7 @@ export default {
 			try {
 				const response = await fetch('/todos');
 				const jsonData = await response.json();
+				console.log(jsonData)
 				use_setTodos(jsonData);
 			} catch (error) {
 				console.error(error.message);
