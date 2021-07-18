c259e5b28dde12a35c0518db06430e503a5c0840
Nicolas <nicolas.lepaj@gmail.com>
2021-07-18 21:52:47 +0200

    fix import

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const Client = require('pg');
+const { Client } = require('pg');
 // require('dotenv').config();
 
 // const devConfig = {
@@ -10,10 +10,10 @@ const Client = require('pg');
 // };
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 client.connect();
