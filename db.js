9e5ab303a939afc9b5c655b35c3306f38fa4f671
Nicolas <nicolas.lepaj@gmail.com>
2021-07-18 21:59:53 +0200

    ssl config

--- db.js
+++ db.js
@@ -12,8 +12,9 @@ const { Client } = require('pg');
 const client = new Client({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
-		rejectUnauthorized: false,
-	},
+		require: true,
+		rejectUnauthorized: false 
+	}
 });
 
 client.connect();
